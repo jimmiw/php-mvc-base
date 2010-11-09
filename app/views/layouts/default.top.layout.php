b45254bf273062c4c22e223a41be24764e8ed243
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2010-11-09 10:49:52 +0100

    Renamed the layout to top.layout. Added a bottom layout. Added the layout array, used in my other projects. Fixed the test index view, so it now uses the new layouts.

--- app/views/layouts/default.top.layout.php
+++ app/views/layouts/default.top.layout.php
@@ -1,10 +1,7 @@
-<?php
-function topLayout($title_for_layout) {
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-	<title><?php echo $title_for_layout; ?> - Static title</title>
+	<title><?php echo $layout['title']; ?> - Static title</title>
 	<meta name="keywords" content="company message private talk" />
 	<meta name="description" content="Post messages privately in your company" />
 	<!--<script type="text/javascript" src="<?php echo get_javascript('jquery-1.4.1.min.js');?>"></script>-->
@@ -12,16 +9,3 @@ function topLayout($title_for_layout) {
 </head>
 <body>
 
-<?php
-} // topLayout ENDS
-?>
-
-<?php
-function bottomLayout() {
-?>
-
-</body>
-</html>
-<?php
-} // bottomLayout ENDS
-?>
--- app/views/test/index.view.php
+++ app/views/test/index.view.php
@@ -1,19 +1,31 @@
 <?php
 
-// including the layout file.
-include('views/layouts/default.layout.php');
+// constructs a layout array, which can hold basically anything.
+//
+// NOTE: Optional!
+// Here i'm just adding a title that is to be used in the top layout file.
+$layout = array(
+  'title' => "Test of php-mvc-base"
+);
 
-// renders the top part of the layout
-// the string given, is the title of the current page
-echo topLayout("Test of php-mvc-base");
+// including the layout file that renders the top part of the page.
+// (headers, title etc)
+include('views/layouts/default.top.layout.php');
 
 ?>
 
-<p>this is the view!</p>
+<p>This written from the view!</p>
 
 <?php 
 
-// Writing out the data sent from the controller
+// Writes data that is "sent" from the controller
+// Actually it's just a variable that was created in the controller, ready to
+// be used in the view. It could be anything.
+// (Used for splitting up the controller / view responsibilities)
 echo "<p>".$some_data."</p>";
+
+
+// including the layout file that renders the bottom part of the page.
+include('views/layouts/default.bottom.layout.php');
 
 ?>
