a0a2a6cff3d982dbc71f798211e0b9ac8d9c85b9
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2010-11-09 10:21:33 +0100

    Replaced the WIP title with "Static title" to better help users use the layouts. Uncommented the javascript include, as it only serves as an example on how to use it.

--- app/views/layouts/default.layout.php
+++ app/views/layouts/default.layout.php
@@ -4,10 +4,10 @@ function topLayout($title_for_layout) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-	<title><?php echo $title_for_layout; ?> - WIP</title>
+	<title><?php echo $title_for_layout; ?> - Static title</title>
 	<meta name="keywords" content="company message private talk" />
 	<meta name="description" content="Post messages privately in your company" />
-	<script type="text/javascript" src="<?php echo get_javascript('jquery-1.4.1.min.js');?>"></script>
+	<!--<script type="text/javascript" src="<?php echo get_javascript('jquery-1.4.1.min.js');?>"></script>-->
 	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet('stylesheet.css'); ?>" />
 </head>
 <body>
