230c022d23524a21f3a68c926b7a170e30497f38
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2012-06-27 20:42:59 +0200

    defined the routes file, where all the routes are read from

---
+++ config/routes.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Used to define the routes in the system.
+ * 
+ * A route should be defined with a key matching the URL and an
+ * controller#action-to-call method. E.g.:
+ * 
+ * '/' => 'index#index',
+ * '/calendar' => 'calendar#index'
+ */
+$routes = array(
+	
+);
