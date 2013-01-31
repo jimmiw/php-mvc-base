20d0aad8dc0082df94f7946c1a8b79140d0a4245
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2013-01-31 21:08:32 +0100

    added a test controller.

---
+++ app/controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+class TestController extends ApplicationController
+{
+	public function indexAction()
+	{
+		$this->view->message = "hello from test::index";
+	}
+	
+	public function checkAction()
+	{
+		echo "hello from test::check";
+	}
+}
