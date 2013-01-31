80c41dfdace4c44d16a7299b121dca5b2da8155d
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2013-01-31 21:07:53 +0100

    using the new Router system

--- web/index.php
+++ web/index.php
@@ -14,11 +14,14 @@ define('CMS_PATH', ROOT_PATH . '/lib/base/');
 // starts the session
 session_start();
 
+// includes the system routes. Define your own routes in this file
+include(ROOT_PATH . '/config/routes.php');
+
 /**
- * Standard autoloader
+ * Standard framework autoloader
  * @param string $className
  */
-function __autoload($className) {
+function autoloader($className) {
 	// controller autoloading
 	if (strlen($className) > 10 && substr($className, -10) == 'Controller') {
 		if (file_exists(ROOT_PATH . '/app/controllers/' . $className . '.php') == 1) {
@@ -29,50 +32,17 @@ function __autoload($className) {
 		if (file_exists(CMS_PATH . $className . '.php')) {
 			require_once CMS_PATH . $className . '.php';
 		}
+		else if (file_exists(ROOT_PATH . '/lib/' . $className . '.php')) {
+			require_once ROOT_PATH . '/lib/' . $className . '.php';
+		}
 		else {
 			require_once ROOT_PATH . '/app/models/'.$className.'.php';
 		}
 	}
 }
 
-// loads the environment settings
-require(ROOT_PATH . '/config/environment.inc.php');
-// loads the database config
-require(ROOT_PATH . '/config/db.inc.php');
+// activates the autoloader
+spl_autoload_register('autoloader');
 
-// includes the system routes
-include(ROOT_PATH . '/config/routes.php');
-
-// tries to find the route and run the given action on the controller
-try {
-	// fetches the current URI
-	$uri = $_SERVER['REQUEST_URI'];
-	$uri = substr($uri, strlen(WEB_ROOT));
-	// if the route isn't defined, try to add a trailing slash
-	if (!isset($routes[$uri])) {
-		$uri .= '/';
-	}
-	// fetches the current route
-	$routeFound = isset($routes[$uri])  ? $routes[$uri] : false;
-
-	// if a matching route was found
-	if ($routeFound) {
-		list($name, $action) = explode('#', $routeFound);
-		// initializes the controller
-		$controller = ucfirst($name) . 'Controller';
-		// constructs the controller
-		$controller = new $controller();
-		// executes the action on the controller
-		$controller->execute($action);
-	}
-	// no route found, throw an exception to run the error controller
-	else {
-		throw new Exception('no route added for ' . $_SERVER['REQUEST_URI']);
-	}
-}
-catch(Exception $exception) {
-	// runs the error controller
-	$controller = new ErrorController();
-	$controller->setException($exception);
-	$controller->execute('error');
-}
+$router = new Router();
+$router->execute($routes);
