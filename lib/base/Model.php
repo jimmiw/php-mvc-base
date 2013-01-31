98b6f12df396cd82a0b86af2ecd7896b7163bc4b
Jimmi Westerberg <jimmi.westerberg@gmail.com>
2013-01-31 21:13:11 +0100

    spaces to tabs. you can now delete an item

--- lib/base/Model.php
+++ lib/base/Model.php
@@ -24,7 +24,8 @@ class Model
 				$settings['database']['dbname']
 			),
 			$settings['database']['user'],
-			$settings['database']['password']
+			$settings['database']['password'],
+			array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 		);
 		
 		$this->init();
@@ -33,6 +34,15 @@ class Model
 	public function init()
 	{
 		
+	}
+	
+	/**
+	 * Sets the database table the model is using
+	 * @param string $table the table the model is using
+	 */
+	protected function _setTable($table)
+	{
+		$this->_table = $table;
 	}
 	
 	public function fetchOne($id)
@@ -46,35 +56,77 @@ class Model
 		return $statement->fetch(PDO::FETCH_OBJ);
 	}
 	
-	/*public function fetchAll($where)
+	/**
+	 * Saves the current data to the database. If an key named "id" is given,
+	 * an update will be issued.
+	 * @param array $data the data to save
+	 * @return int the id the data was saved under
+	 */
+	public function save($data = array())
 	{
-		$sql = 'select * from ';
-		$sql .= $this->_table;
-		$sql .= ' where';
+		$sql = '';
 		
-		$parameters = array();
-
-		if (is_array($data)) {
-			$first = true;
+		$values = array();
+		
+		if (array_key_exists('id', $data)) {
+			$sql = 'update ' . $this->_table . ' set ';
 			
-			foreach ($where as $key => $value) {
-				
-				if ($key == null) {
-					$sql .= ' ' . $value;
+			$first = true;
+			foreach($data as $key => $value) {
+				if ($key != 'id') {
+					$sql .= ($first == false ? ',' : '') . ' ' . $key . ' = ?';
+					
+					$values[] = $value;
+					
+					$first = false;
 				}
-				else {
-					$sql .= ' $key = ?';
-				}
+			}
+			
+			// adds the id as well
+			$values[] = $data['id'];
+			
+			$sql .= ' where id = ?';// . $data['id'];
+			
+			$statement = $this->_dbh->prepare($sql);
+			return $statement->execute($values);
+		}
+		else {
+			$keys = array_keys($data);
+			
+			$sql = 'insert into ' . $this->_table . '(';
+			$sql .= implode(',', $keys);
+			$sql .= ')';
+			$sql .= ' values (';
+			
+			$dataValues = array_values($data);
+			$first = true;
+			foreach($dataValues as $value) {
+				$sql .= ($first == false ? ',?' : '?');
 				
+				$values[] = $value;
 				
 				$first = false;
 			}
-		}
-		else {
-			$sql .= ' id = ' . $where;
+			
+			$sql .= ')';
+			
+			$statement = $this->_dbh->prepare($sql);
+			if ($statement->execute($values)) {
+				return $this->_dbh->lastInsertId();
+			}
 		}
 		
-		$statement = $this->_dbh->prepare($sql);
-		$statement->execute($parameters);
-	}*/
+		return false;
+	}
+	
+	/**
+	 * Deletes a single entry
+	 * @param int $id the id of the entry to delete
+	 * @return boolean true if all went well, else false.
+	 */
+	public function delete($id)
+	{
+		$statement = $this->_dbh->prepare("delete from " . $this->_table . " where id = ?");
+		return $statement->execute(array($id));
+	}
 }
